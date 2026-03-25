f4fbc26fc3d742a56962ff6b1ca10cee1e27a886
FOA-developer <flourishagbanoma@gmail.com>
2026-03-25 04:07:03 +0100

    home component

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return ( 
+    <div className = "pt-20">
+      <h3 className="font-inter font-semibold text-xs">EXECUTION  INTELLIGENCE ORANGIZATION</h3>
+      <h1 className="">Execution fails more than<span className="">strategy. We study</span><span className="font-display italic"> why.</span></h1>
+      <p className="">ThePMForge brings together experienced project and program leaders to uncover real execution breakdowns and turn them into practical insights, frameworks, and solutions</p>
+      <div className="flex flex-row gap-10">
+        <button className="">Request an Invite</button>
+        <button className="">Join The Waitlist</button>
+      </div>
+    </div>
+   );
+}
+ 
+export default Home;
