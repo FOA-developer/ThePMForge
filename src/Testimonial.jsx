2ca0230ab8c2b57a92d57b9935e8712a8c697435
FOA-developer <flourishagbanoma@gmail.com>
2026-03-26 13:00:14 +0100

    ADDED THESE NEW COMPONENTS

---
+++ src/Testimonial.jsx
@@ -0,0 +1,9 @@
+const Testimonial = () => {
+  return ( 
+    <div className="bg-white">
+      <h3 className="font-inter font-semibold text-xs text-[rgb(1,104,126)]">EARLY SIGNALS</h3>
+    </div>
+   );
+}
+ 
+export default Testimonial;
