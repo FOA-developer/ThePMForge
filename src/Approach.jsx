39d2571daa348f8a0b3d3a46e8190019ccc660fb
FOA-developer <flourishagbanoma@gmail.com>
2026-03-26 12:46:24 +0100

    added more content

--- src/Approach.jsx
+++ src/Approach.jsx
@@ -1,20 +1,20 @@
 const Approach = () => {
   return ( 
-    <div className="bg-[rgb(2,44,83)] text-white flex flex-col gap-15 pl-6">
+    <div className="bg-[rgb(2,44,83)] text-white flex flex-col gap-15 pl-6 pr-6 pb-20">
       <div className="pt-20 ">
         <h3 className="font-inter text-xs font-semibold text-[rgb(1,104,126)]">THE PROBLEM</h3>
-        <ul className=" pl-5 pt-5 flex flex-col gap-3">
-          <li className="font-display font-medium text-lg">Projects fail despite solid plans.</li>
-          <li className="font-display font-medium text-lg">Teams misalign mid-execution.</li>
-          <li className="font-display font-medium text-lg">Stakeholders derail delivery.</li>
-          <li className="font-display font-medium text-lg">Frameworks don't solve real-world complexity.</li>
+        <ul className=" pl-5 pt-5 flex flex-col gap-3 font-display font-medium text-lg">
+          <li>Projects fail despite solid plans.</li>
+          <li>Teams misalign mid-execution.</li>
+          <li>Stakeholders derail delivery.</li>
+          <li>Frameworks don't solve real-world complexity.</li>
         </ul>
         <h2 className="italic font-display font-semibold text-lg pt-3 pl-5 text-[rgb(1,104,126)]">The problem isn't planning. It's execution.</h2>
       </div>
       <div className="pt-6" >
         <h3 className="font-inter text-xs font-semibold text-[rgb(1,104,126)]">WHO WE ARE</h3>
         <h2 className="font-display font-medium pt-5 pl-5 text-2xl">ThePMForge is an <span className="font-bold"> execution intelligence </span>organization.</h2>
-        <p className="text-[rgb(1,104,126)] font-inter pl-5 pt-3 text-sm">We study real execution what breaks, why it breaks, and how experienced operators navigate it.</p>
+        <p className="text-[rgb(1,104,126)] font-inter font-semibold pl-5 pt-3 text-sm">We study real execution what breaks, why it breaks, and how experienced operators navigate it.</p>
         <div className="flex flex-col gap-4 pt-6">
           <div className="flex flex-row gap-3 items-center pl-5">
             <div className="w-1 h-6 bg-[rgb(1,104,126)]" ></div>
@@ -27,10 +27,10 @@ const Approach = () => {
        </div>
      </div>
      <div>
-        <h3 className="font-inter text-xs font-semibold text-[rgb(1,104,126)]">THE SYSTEM</h3>
-        <p className="text-[rgb(1,104,126)] font-inter pl-5 pt-5 text-sm">We operate through a system.</p>
+        <h3 className="font-inter text-xs font-semibold text-[rgb(1,104,126)] pt-5">THE SYSTEM</h3>
+        <p className="text-[rgb(1,104,126)] font-inter font-semibold pl-5 pt-5 text-sm">We operate through a system.</p>
         <div className="flex flex-col gap-6 pt-6">
-          <div className="ml-10 bg-white w-lg pr-2 h-30 border-3 mb-2 border-[rgb(1,104,126)] rounded-xl flex flex-row gap-4 items-center pl-5">
+          <div className="ml-2 bg-white w-80 pr-2 h-30 border-3 mb-2 border-[rgb(1,104,126)] rounded-xl flex flex-row gap-4 items-center pl-5">
             <div className="font-semibold font-inter text-sm text-[rgb(1,104,126)] rounded-full bg-[rgb(229,240,242)] p-3">
               01
             </div>
@@ -39,7 +39,7 @@ const Approach = () => {
               <p className="text-[rgb(1,104,126)] font-inter text-xs">Small, invite-only sessions with experienced operators sharing unfiltered delivery realities.</p>
             </div>
           </div>
-          <div className="ml-10 bg-white w-lg pr-2 h-30 border-3 mb-2 border-[rgb(1,104,126)] rounded-xl flex flex-row gap-4 items-center pl-5">
+          <div className="ml-2 bg-white w-80 pr-2 h-30 border-3 mb-2 border-[rgb(1,104,126)] rounded-xl flex flex-row gap-4 items-center pl-5">
             <div className="font-semibold font-inter text-sm text-[rgb(1,104,126)] rounded-full bg-[rgb(229,240,242)] p-3">
               02
             </div>
@@ -48,7 +48,7 @@ const Approach = () => {
               <p className="text-[rgb(1,104,126)] font-inter text-xs">Capturing recurring patterns, failure modes, and the key decisions that changed outcomes.</p>
             </div>
           </div>
-          <div className="ml-10 bg-white w-lg h-30 border-3 mb-2 pr-2 border-[rgb(1,104,126)] rounded-xl flex flex-row gap-4 items-center  pl-5">
+          <div className="ml-2 bg-white w-80 h-30 border-3 mb-2 pr-2 border-[rgb(1,104,126)] rounded-xl flex flex-row gap-4 items-center  pl-5">
             <div className="font-semibold font-inter text-sm text-[rgb(1,104,126)] rounded-full bg-[rgb(229,240,242)] p-3">
               03
             </div>
@@ -59,13 +59,21 @@ const Approach = () => {
           </div>
         </div>
      </div>
-     <div className="pt- ">
+     <div className="pt-6">
         <h3 className="font-inter text-xs font-semibold text-[rgb(1,104,126)]">WHAT YOU GET</h3>
-        <ul className="list-dist pl-5 pt-5 flex flex-col gap-3">
-          <li className="font-display font-medium text-lg">Execution-focused roundtable discussions</li>
-          <li className="font-display font-medium text-lg">Practical frameworks for delivery challenges</li>
-          <li className="font-display font-medium text-lg">Stakeholders derail delivery.</li>
-          <li className="font-display font-medium text-lg">Support for diagnosing struggling projects</li>
+        <ul className="list-disc ml-5 font-display font-medium pt-6 flex flex-col gap-3 tracking-wide pr-3 mr-2">
+          <li>Execution-focused roundtable discussions</li>
+          <li>Practical frameworks for delivery challenges</li>
+          <li>Stakeholders derail delivery.</li>
+          <li>Support for diagnosing struggling projects</li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="pt-4 font-inter text-xs font-semibold text-[rgb(1,104,126)]">WHO IS THIS FOR</h3>
+        <ul className="pl-5 pt-6 flex flex-col gap-3 font-display font-medium tracking-wide pr-3 mr-2">
+          <li className="before:content-['✓'] before:mr-2">Project Managers delivering complex work</li>
+          <li className="before:content-['✓'] before:mr-2">Program Managers handling cross-functional execution</li>
+          <li className="before:content-['✓'] before:mr-2">Operators responsible for outcomes</li>
         </ul>
       </div>
     </div>
