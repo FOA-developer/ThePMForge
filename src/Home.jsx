ef313338994ae532beea3d5df4c5123564df6c7b
FOA-developer <flourishagbanoma@gmail.com>
2026-04-07 16:57:26 +0100

    Corrected a typo

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return ( 
     <div className="pt-20 pl-6 pr-2 pb-24 md:w-[80%] md:pl-20 lg:w-[70%]">
-      <h3 className="font-inter font-semibold text-xs text-[rgb(11,41,95)]">EXECUTION  INTELLIGENCE ORANGIZATION</h3>
+      <h3 className="font-inter font-semibold text-xs text-[rgb(11,41,95)]">EXECUTION  INTELLIGENCE ORANGNIZATION</h3>
       <h1 className="font-display font-bold text-6xl pt-5 text-[rgb(11,41,95)] ">Execution fails more than strategy. We study<span className="font-display italic"> why.</span></h1>
       <p className="font-inter pt-4 text-base text-[rgb(11,41,95)]">ThePMForge brings together experienced project and program operators in curated roundtables to uncover real execution breakdowns and turn them into structured, practical systems for better delivery.</p>
       <div className="flex flex-row gap-5 pt-10 items-center [rgb(11,41,95)]">
