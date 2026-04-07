7a2ec9433cbce15ffe38be065f0ab042a33a2dea
FOA-developer <flourishagbanoma@gmail.com>
2026-04-07 14:56:39 +0100

    fixed padding left issue

--- src/About.jsx
+++ src/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         <p className="font-inter font-semibold text-sm text-[rgb(220,249,253)]  pt-3 hidden md:block">Oghosa Agbonlahor, Founder</p>
       </div>
       <p className="font-inter font-semibold text-sm text-[rgb(220,249,253)]  pt-3 md:hidden ">Oghosa Agbonlahor, Founder</p>
-      <div className="w-full md:w-[60%] lg:w-[70%] max-w-[820px] text-left md:pb-3 pt-10 md:pt-20">
+      <div className="w-full md:w-[60%] lg:w-[70%] max-w-[820px] text-left pl-6 md:pb-3 pt-10 md:pt-20">
         <h3 className="font-inter font-medium text-xs text-[rgb(220,249,253)]">FOUNDED ON EXPERIENCE</h3>
         <h1 className="font-display font-semibold text-2xl pt-5 pb-6">Built From Real <span className="font-display italic">Execution Experience.</span></h1>
         <p className="font-inter pl-2 pb-10 font-light"><span className="font-semibold"> Oghosa Agbonlahor</span> is an execution operator focused on one thing: understanding why projects fail in the real world.<br></br><br></br>He has supported 200+ training sessions reaching 5,000+ learners, working inside high-pressure delivery environments where execution, not planning,determines outcomes.<br></br><br></br>ThePMForge is built from that reality: bringing operators together to break down real execution failures and convert them into practical systems.<br></br><br></br>Because the problem isn’t planning. It’s execution.</p>
