d5adbd733881da3b7aa42df795a1d4b6900ddc36
FOA-developer <flourishagbanoma@gmail.com>
2026-04-14 13:29:22 +0100

    new coponent

---
+++ src/Navmenu.jsx
@@ -0,0 +1,13 @@
+const Navmenu = () => {
+  return (
+    <nav className="navmenu">
+      <ul className="navmenu-list">
+        <li><a href="#approach">Approach</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="https://docs.google.com/forms/d/e/1FAIpQLScGlUTQCNadu03JH7oueH5JjgLxs8OPcFwn-KVr2nfTjYqcsw/viewform?usp=dialog" target="_blank" rel="noopener noreferrer"><button className="request-invite-button">Request an Invite</button></a></li>
+      </ul>
+    </nav>
+   );
+}
+ 
+export default Navmenu;
